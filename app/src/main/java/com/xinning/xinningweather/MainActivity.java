0f9ba5846e3259f3a957f64b5ab8a29de34ae694
guoxi <guoxinqi94@outlook.com>
2017-08-13 20:02:19 +0800

    第一次提交

---
+++ app/src/main/java/com/xinning/xinningweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xinning.xinningweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 将会仿照第一行代码中的最后一个一个项目来进行学习
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
